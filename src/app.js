beba98db7e5d1ca0de2836221458919d06bb8159
first <msonam189@gmail.com>
2024-11-27 18:07:48 +0530

    add register controller and bug file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import expree from "express"
+import express from "express"
 import cors from "cors" 
 import cookieParser from "cookie-parser"
 
@@ -13,5 +13,16 @@ app.use(express.json({limit: "10kb"}))
 app.use(express.urlencoded({extended: true,limit: "10kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+//import routes
+import userRouter  from './routes/user.routes.js'
+
+
+//routes declaeration
+ app.use("/api/v1/users",userRouter)
+
+ // http://localhost:8000/api/v1/users/register
 
 export {app }
